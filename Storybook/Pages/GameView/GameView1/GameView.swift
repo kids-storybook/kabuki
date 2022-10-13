951d3d67a65c6e264f04ef65ad25c38cb746156b
Adam Fiadi <adamfiadi@gmail.com>
2022-10-13 14:06:34 +0700

    add codable spritekit in gameview1 with getNextScene

--- Storybook/Pages/GameView/GameView1/GameView.swift
+++ Storybook/Pages/GameView/GameView1/GameView.swift
@@ -14,6 +14,7 @@ class GameView: GameScene {
     let lionMom = SKSpriteNode(imageNamed: "ibuSinga")
     let lionDad = SKSpriteNode(imageNamed: "bapakSinga")
     let backgroundScene = SKSpriteNode(imageNamed: "GameBgImg")
+    let nextBtn = SKSpriteNode(imageNamed: "nextBtn")
 
     private func setupPlayer(){
         backgroundScene.position = CGPoint(x: frame.midX, y: frame.midY)
@@ -28,18 +29,37 @@ class GameView: GameScene {
         lionDad.position = CGPoint(x: -frame.midX/2-100, y: -frame.midY/2-100)
         lionDad.size = CGSize(width: 585, height: 453)
         
+        nextBtn.position = CGPoint(x: frame.maxX-100, y: -frame.maxY/2-100)
+        nextBtn.size = CGSize(width: 50, height: 68)
+        
         addChild(backgroundScene)
         addChild(lionCub)
         addChild(lionMom)
         addChild(lionDad)
+        addChild(nextBtn)
+        
     }
     
     override func didMove(to view: SKView) {
         self.setupPlayer()
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        let touch = touches.first
+        if let location = touch?.location(in: self) {
+            if nextBtn.contains(location) {
+                goToScene(scene: getNextScene()!)
+            }
+        }
+        
     }
     
     override func getNextScene() -> SKScene? {
       return SKScene(fileNamed: "GameView2") as! GameView2
     }
+    
+    
     
 }
