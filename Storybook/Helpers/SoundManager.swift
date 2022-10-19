259efdff9cb6a5f5032ccaaae5d4c64babc5c77a
ghozy.afif <ghozy.afif@prosa.ai>
2022-10-19 15:39:51 +0700

    Update, implementation of map view

--- Storybook/Helpers/SoundManager.swift
+++ Storybook/Helpers/SoundManager.swift
@@ -10,15 +10,7 @@ import SpriteKit
 
 class SoundManager {
     
-    let soundPew = SKAction.playSoundFileNamed("pew.wav", waitForCompletion: false)
-    let soundPew2 = SKAction.playSoundFileNamed("pew2.wav", waitForCompletion: false)
-    let soundBoom = SKAction.playSoundFileNamed("boom.wav", waitForCompletion: false)
-    let soundSpawn = SKAction.playSoundFileNamed("spawn.wav", waitForCompletion: false)
-    let soundAttack = SKAction.playSoundFileNamed("attack.wav", waitForCompletion: false)
-    let soundDefend = SKAction.playSoundFileNamed("defend.wav", waitForCompletion: false)
-    let soundBigHit = SKAction.playSoundFileNamed("bigHit.wav", waitForCompletion: false)
-    let soundSmallHit = SKAction.playSoundFileNamed("smallHit.wav", waitForCompletion: false)
-    let soundClickedButton = SKAction.playSoundFileNamed("button-click.mp3", waitForCompletion: false)
+    let soundClickedButton = SKAction.playSoundFileNamed("button-click.wav", waitForCompletion: false)
     
     static let sharedInstance = SoundManager()
     
